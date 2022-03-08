82352ef95cf97e0323aca682ab39a295fe2d7a42
albert-de-montserrat <58044444+albert-de-montserrat@users.noreply.github.com>
2022-03-08 15:20:40 +0100

    Add files via upload

---
+++ src/BallsGen.jl
@@ -0,0 +1,72 @@
+module BallsGen
+
+export generate_inclusions
+
+# sphere volume
+vol(r) = 4/3*π*r^3
+
+# random inclusoin center
+new_center(lx, ly, lz) = (rand()*lx, rand()*ly, rand()*lz)
+
+# 3D euclidean distance
+distance(p1, p2) = sqrt((p1[1]-p2[1])^2 + (p1[2]-p2[2])^2 + (p1[3]-p2[3])^2)
+
+# check if the new inclusion collides with any pre-existent inclusion
+function sphere_collision(coords, new_coord, r)
+    for c in coords
+        if distance(c, new_coord) ≤ 2*r*1.2
+            return true
+        end
+    end
+
+    return false
+end
+
+function wall_collision(new_coord, r, lx, ly, lz)
+    x, y, z = new_coord
+
+    if (x ≤ r*1.2) || (x ≥ (lx - r*1.2))
+        return true
+
+    elseif (y ≤ r*1.2) || (y ≥ (ly - r*1.2))
+        return true
+    
+    elseif (z ≤ r*1.2) || (z ≥ (lz - r*1.2))
+        return true
+    end
+
+    return false
+end
+
+function generate_inclusions(lx, ly, lz, ϕ, r)
+    # first sphere coordinate
+    coords = [new_center(lx, ly, lz)]
+
+    tot_vol = vol(r)
+    vol_fraction = tot_vol/(lx*ly*lz)
+
+    iter = 0
+    while vol_fraction ≤ ϕ
+        iter += 1
+        # temptative coordinate
+        new_coord = new_center(lx, ly, lz) 
+        collision1 = sphere_collision(coords, new_coord, r)
+        collision2 = wall_collision(new_coord, r, lx, ly, lz)
+        if !collision1 && !collision2
+            push!(coords, new_coord)
+            tot_vol += vol(r)
+            vol_fraction = tot_vol/(lx*ly*lz)
+            iter = 0
+        end
+        if iter > 500
+            println("Forcing exit. Generator stalling")
+            break
+        end
+    end
+
+    println("Done with $(length(coords)) inclusions and $(vol_fraction*100) % volume fraction.")
+
+    return coords, vol_fraction
+end
+
+end # module
